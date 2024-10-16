a73374a6ec2d6840932d0f2e9917d42438eb9362
RobertinoLirusso <robertinolirusso3@gmail.com>
2024-10-16 19:19:03 -0300

    chernobyl spa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    title: 'Chernobyl Disaster'
+  },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
